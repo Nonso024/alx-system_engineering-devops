28c0cc5dc8ff31c272b4afb827ff9c2ef3315648
Nonso024 <josephani772@gmail.com>
2023-06-15 20:25:44 +0100

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,6 @@
-# Increases the amount of traffic an Nginx server can handle.
+# increase the limit on open files to allow requests to be handled
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec {'sedstart':
+  provider => shell,
+  command  => 'sudo sed -i "s/15/4000/" /etc/default/nginx;sudo service nginx restart'
 }
